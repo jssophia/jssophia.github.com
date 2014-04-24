e7d4911093cf2b66a3e748204e48a6a2139715c3
Hugo Mallet <hugomallet06@gmail.com>
2014-04-24 16:12:13 +0200

    Add Twitter and Meetup buttons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
+(function() {
+	'use strict';
+
+	function addTweetButton($session) {
+			var $title = $session.find('h3'),
+			$date = $title.find('.date'),
+			title = $title[0].firstChild.data,
+			hastag = $session.data('hashtag') || '#JSSophia',
+
+			button = '<a href="https://twitter.com/share" class="twitter-share-button" data-via="JSSophia" ' +
+				'data-lang="fr" data-size="large" data-count="none">Tweeter</a>',
+			$button = $(button);
+		
+		$button.attr('data-text', 'Prochaine session: ' + hastag + ' le ' + $date.text() + ' -> jssophia.github.com');
+		$title.after($button);
+	}
+
+	function addMeetupButton($session) {
+		var $title = $session.find('h3'),
+			meetup = $session.data('meetup'),
+			
+			button = '<a href="http://www.meetup.com/FranceJS/events/' + meetup + '/" class="mu-rsvp-btn">Meetup</a>',
+			$button = $(button);
+
+		$button.attr('data-event',  meetup);
+		$title.after($button);
+	}
+
+	$(function() {
+
+		var $sessions = $('#sessions'),
+			$nextSession = $sessions.find('article').first();
+
+		addTweetButton($nextSession);
+		addMeetupButton($nextSession);
+	});
+
+
+})();
 
