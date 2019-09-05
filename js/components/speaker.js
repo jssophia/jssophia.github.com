8d702c9584dc451ea24588f69d348cdf2961a0bf
jmlegait <jean-michael.legait@crossknowledge.com>
2019-09-05 17:36:37 +0200

    Fix attributes

--- js/components/speaker.js
+++ js/components/speaker.js
@@ -1,33 +1,34 @@
 class Speaker extends HTMLElement {
 
-    constructor(twitter, github) {
+    constructor() {
         super();
         this.name = this.getAttribute('name');
         this.twitter = this.getAttribute('twitter');
         this.github = this.getAttribute('github');
+        this.avatar = this.getAttribute('avatar');
     }
 
     connectedCallback() {
-        this.innerHTML = `<div>
-        <div class="speaker__name">${this.name}</div>
-        <ul>
-          <li>
-            Twitter:
-            <a href="https://twitter.com/${this.twitter}">@${this.twitter}</a>
-          </li>
-          <li>
-            Github:
-            <a href="https://github.com/${this.github}">${this.github}</a>
-          </li>
-        </ul>
-        <div class="talk__slide">
-            <a
-              href="https://speakerdeck.com/mariusmez/voyagez-avec-tensorflow-dot-js">
-              Slides
-            </a>
+        this.innerHTML = `
+        <div class="speaker__avatarWrapper">
+            <img
+            class="speaker__avatar"
+            src="${this.avatar}"
+            alt=""/>
         </div>
-      </div>`
-      this.innerHTML = this.shadowRoot.innerHTML;
+        <div>
+            <div class="speaker__name">${this.name}</div>
+            <ul>
+            <li>
+                Twitter:
+                <a href="https://twitter.com/${this.twitter}">@${this.twitter}</a>
+            </li>
+            <li>
+                Github:
+                <a href="https://github.com/${this.github}">${this.github}</a>
+            </li>
+            </ul>
+        </div>`;
     }
 }
 
